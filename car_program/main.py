c6a9f3e7e41ebbccf8f9b2cc0749af51ac770dbc
Saulius Sabaitis <s.sabaitis@gmail.com>
2022-03-14 22:13:41 +0200

    added das_print function

--- car_program/main.py
+++ car_program/main.py
@@ -26,44 +26,47 @@ cars = [
     Car("Jeep", "Whatewer", 200)
 ]
 
+def dash_print():
+    print("----------------------------------")
+
 # 1 all cars that is less than 120 euros
 
 car_les100 = list(filter(lambda x: x.price < 120, cars))
 print("Cars with a price less than 120:")
 for i in car_les100:
     print(i.make, i.model, i.price)
-print("----------------------------------")
+dash_print()
 
 # 2. cars above 300 euros
 car_more300 = list(filter(lambda x: x.price > 300, cars))
 print("Cars with a price more than 300:")
 for i in car_more300:
     print(i.make, i.model, i.price)
-print("----------------------------------")
+dash_print()
 
 # 3. what is the cheapest price for "ford" car
 car_ford = list(filter(lambda x: x.make == "Ford", cars))
 cheap_ford = reduce(lambda x, y: x if x.price < y.price else y, car_ford)
 print("Cheapest Ford car:")
 print(cheap_ford.make, cheap_ford.model, cheap_ford.price)
-print("----------------------------------")
+dash_print()
 
 # 4. the most expensive car in the list
 expensive = reduce(lambda x, y: y if x.price < y.price else x, cars)
 print("Most expensive car:")
 print(expensive.make, expensive.model, expensive.price)
-print("----------------------------------")
+dash_print()
 
 # 5. total cost of the "ford" cars in the list
 car_ford = list(filter(lambda x: x.make == "Ford", cars))
 print("Sum of all Ford cars:")
 suma = sum(x.price for x in car_ford)
 print(suma)
-print("----------------------------------")
+dash_print()
 
 # 6. Total count of the "Jeep" car in the list
 car_jeep = list(filter(lambda x: x.make == "Jeep", cars))
 print("Total of all Jeep cars:")
 number_of_jeep_cars = len(car_jeep)
 print(number_of_jeep_cars)
-print("----------------------------------")
+dash_print()
